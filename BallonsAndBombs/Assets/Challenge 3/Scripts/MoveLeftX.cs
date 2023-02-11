2ed28a8bd5c3817cfadc3530e38954f0516e490e
Elmar055 <104317952+Elmar055@users.noreply.github.com>
2023-02-11 23:16:28 +0400

    All files upload

---
+++ BallonsAndBombs/Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftX : MonoBehaviour
+{
+    // speed for background and money and bomb
+    public float speed;
+    // for use PlayerController class'es variables and methods
+    private PlayerControllerX playerControllerScript;
+    // left limit for objects
+    private float leftBound = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // to give Player object's PlayerController component to playerControllerScript
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If game is not over, move to the left
+        if (!playerControllerScript.gameOver)
+        {
+            // move left
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+
+        // If object goes off screen that is NOT the background, destroy it
+        if (transform.position.x < leftBound && !gameObject.CompareTag("Background"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ BallonsAndBombs/Assets/Challenge 3/Scripts/SpawnManagerX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    // gameobject array for prefabs
+    public GameObject[] objectPrefabs;
+    // start after spawnDelay
+    private float spawnDelay = 2;
+    // interval
+    private float spawnInterval = 1.5f;
+
+    // for use PlayerController class'es variables and methods
+    private PlayerControllerX playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", spawnDelay, spawnInterval);
+
+        // to give Player object's PlayerController component to playerControllerScript
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+
+    // Spawn obstacles
+    void SpawnObjects ()
+    {
+        // Set random spawn location and random object index
+        Vector3 spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
+        // random index for objectprefabs array
+        int index = Random.Range(0, objectPrefabs.Length);
+
+        // if game is still active, spawn new object
+        if (!playerControllerScript.gameOver)
+        {
+            Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+        }
+
+    }
+}
